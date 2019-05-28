de70418bc89b7ab0a1baa245dd41aa21e46a4801
Jeremy E Kozdon <jekozdon@nps.edu>
2019-05-27 21:43:53 -0700

    Merge interpolate.py and initial.py

--- interpolate.py
+++ interpolate.py
@@ -1,3 +1,17 @@
+from vtk.numpy_interface import dataset_adapter as dsa
+from vtk.numpy_interface import algorithms as algs
+
+pdi = self.GetInput()
+pdo = self.GetOutput()
+nelem = pdi.GetNumberOfCells()
+Nq = pdi.GetCell(0).GetOrder(0)+1
+if pdi.GetCell(0).GetNumberOfPoints() == Nq * Nq:
+    Nqk = 1
+elif pdi.GetCell(0).GetNumberOfPoints() == Nq * Nq * Nq:
+    Nqk = Nq
+else:
+    raise Exception('cannot determine dimension')
+
 def interpolate(Nq, Nqk, r, w, fld, tmp, nelem):
     fld = reshape(fld, (Nq, Nq, Nqk, nelem), order='F')
     h = 2/Nq
