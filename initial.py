7328bf0a4b3c3ec3e36ecb26f66552e2369df325
Jeremy E Kozdon <jekozdon@nps.edu>
2019-05-27 21:38:03 -0700

    Fix interpolate for 2D

--- initial.py
+++ initial.py
@@ -5,3 +5,9 @@ pdi = self.GetInput()
 pdo = self.GetOutput()
 nelem = pdi.GetNumberOfCells()
 Nq = pdi.GetCell(0).GetOrder(0)+1
+if pdi.GetCell(0).GetNumberOfPoints() == Nq * Nq:
+    Nqk = 1
+elif pdi.GetCell(0).GetNumberOfPoints() == Nq * Nq * Nq:
+    Nqk = Nq
+else:
+    raise Exception('cannot determine dimension')
--- interpolate.py
+++ interpolate.py
@@ -1,9 +1,9 @@
-def interpolate(Nq, r, w, fld, tmp, nelem):
-    fld = reshape(fld, (Nq, Nq, Nq, nelem), order='F')
+def interpolate(Nq, Nqk, r, w, fld, tmp, nelem):
+    fld = reshape(fld, (Nq, Nq, Nqk, nelem), order='F')
     h = 2/Nq
     for e in range(nelem):
         # interpolate in r
-        for k in range(Nq):
+        for k in range(Nqk):
             for j in range(Nq):
                 for i in range(Nq):
                     re = i * 2.0 / (Nq - 1) - 1
@@ -21,7 +21,7 @@ def interpolate(Nq, r, w, fld, tmp, nelem):
                 for i in range(Nq):
                     fld[i, j, k, e] = tmp[i]
         # interpolate in s
-        for k in range(Nq):
+        for k in range(Nqk):
             for i in range(Nq):
                 for j in range(Nq):
                     re = j * 2.0 / (Nq - 1) - 1
@@ -39,23 +39,24 @@ def interpolate(Nq, r, w, fld, tmp, nelem):
                 for j in range(Nq):
                     fld[i, j, k, e] = tmp[j]
         # interpolate in t
-        for i in range(Nq):
-            for j in range(Nq):
-                for k in range(Nq):
-                    re = k * 2.0 / (Nq - 1) - 1
-                    numx = 0.0
-                    den = 0.0
-                    for n in range(Nq):
-                        if re == r[n]:
-                            numx = fld[i, j, n, e]
-                            den = 1
-                            break
-                        val = w[n] / (re - r[n])
-                        numx = numx + fld[i, j, n, e] * val
-                        den = den + val
-                    tmp[k] = numx / den
-                for k in range(Nq):
-                    fld[i, j, k, e] = tmp[k]
+        if Nqk > 1:
+            for i in range(Nq):
+                for j in range(Nq):
+                    for k in range(Nq):
+                        re = k * 2.0 / (Nq - 1) - 1
+                        numx = 0.0
+                        den = 0.0
+                        for n in range(Nq):
+                            if re == r[n]:
+                                numx = fld[i, j, n, e]
+                                den = 1
+                                break
+                            val = w[n] / (re - r[n])
+                            numx = numx + fld[i, j, n, e] * val
+                            den = den + val
+                        tmp[k] = numx / den
+                    for k in range(Nq):
+                        fld[i, j, k, e] = tmp[k]
 
 # LGL points and barycentric weight (as computed by chebfun)
 if Nq == 2:
@@ -87,26 +88,26 @@ elif Nq == 10:
     w = array([-2.604724104587959e-01,     6.379590749419513e-01,    -8.286142980093549e-01,     9.442590400325345e-01,    -1.000000000000000e+00,     1.000000000000000e+00,    -9.442590400325345e-01,     8.286142980093549e-01,    -6.379590749419513e-01,     2.604724104587959e-01,])
 
 newPoints = vtk.vtkPoints()
-x = empty([Nq, Nq, Nq, 1])
-y = empty([Nq, Nq, Nq, 1])
-z = empty([Nq, Nq, Nq, 1])
+x = empty([Nq, Nq, Nqk, 1])
+y = empty([Nq, Nq, Nqk, 1])
+z = empty([Nq, Nq, Nqk, 1])
 tmp = empty(Nq)
 print("interp xyz")
 for e in range(nelem):
-    for k in range(Nq):
+    for k in range(Nqk):
         for j in range(Nq):
             for i in range(Nq):
-                n = i + j * Nq + k * Nq * Nq + e * Nq * Nq * Nq
-                coord = pdi.GetPoint(i + j * Nq + k * Nq * Nq + e * Nq * Nq * Nq)
+                n = i + j * Nq + k * Nqk * Nq + e * Nqk * Nq * Nq
+                coord = pdi.GetPoint(i + j * Nq + k * Nqk * Nq + e * Nqk * Nq * Nq)
                 x[i, j, k, 0], y[i, j, k, 0], z[i, j, k, 0] = coord[:3]
-    interpolate(Nq, r, w, x, tmp, 1)
-    interpolate(Nq, r, w, y, tmp, 1)
-    interpolate(Nq, r, w, z, tmp, 1)
+    interpolate(Nq, Nqk, r, w, x, tmp, 1)
+    interpolate(Nq, Nqk, r, w, y, tmp, 1)
+    interpolate(Nq, Nqk, r, w, z, tmp, 1)
 
-    for k in range(Nq):
+    for k in range(Nqk):
         for j in range(Nq):
             for i in range(Nq):
-                n = i + j * Nq + k * Nq * Nq + e * Nq * Nq * Nq
+                n = i + j * Nq + k * Nqk * Nq + e * Nqk * Nq * Nq
                 newPoints.InsertPoint(n, x[i, j, k, 0], y[i, j, k, 0], z[i, j, k, 0])
 
 pdo.SetPoints(newPoints)
@@ -116,5 +117,5 @@ np_do = dsa.WrapDataObject(pdo)
 for fldname in np_di.PointData.keys():
     print(fldname)
     fld = copy(np_di.PointData[fldname])
-    interpolate(Nq, r, w, fld, tmp, size(fld) / (Nq * Nq * Nq))
+    interpolate(Nq, Nqk, r, w, fld, tmp, size(fld) / (Nq * Nq * Nqk))
     np_do.PointData.append(fld, fldname)
