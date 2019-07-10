2ac0ba613ee30c3b34dfcf796c3c6da6a842eff3
Jeremy E Kozdon <jekozdon@nps.edu>
2019-07-09 20:38:33 -0700

    Add check of points

--- generate_data.jl
+++ generate_data.jl
@@ -5,6 +5,64 @@ catch
   include("quadrature.jl")
 end
 
+# From the VTK source:
+# https://gitlab.kitware.com/nick.laurenson/vtk/blob/d8aa5b89c622cf04b3322112fda420afc9d2a16d/Common/DataModel/vtkLagrangeHexahedron.cxx#L729-795
+function PointIndexFromIJK(i, j, k, N)
+  ibdy = (i == 0 || i == N)
+  jbdy = (j == 0 || j == N)
+  kbdy = (k == 0 || k == N)
+  # How many boundaries do we lie on at once?
+  nbdy = (ibdy ? 1 : 0) + (jbdy ? 1 : 0) + (kbdy ? 1 : 0)
+
+  if (nbdy == 3) # Vertex DOF
+    return (i != 0 ? (j != 0 ? 2 : 1) : (j != 0 ? 3 : 0)) + (k != 0 ? 4 : 0)
+  end
+
+  offset = 8
+  if (nbdy == 2) # Edge DOF
+    if (!ibdy)
+      # On i axis
+      return (i - 1) +
+      (j != 0 ? N - 1 + N - 1 : 0) +
+      (k != 0 ? 2 * (N - 1 + N - 1) : 0) +
+      offset
+    end
+    if (!jbdy)
+      # On j axis
+      return (j - 1) +
+      (i != 0 ? N - 1 : 2 * (N - 1) + N - 1) +
+      (k != 0 ? 2 * (N - 1 + N - 1) : 0) +
+      offset
+    end
+    # !kbdy, On k axis
+    offset += 4 * (N - 1) + 4 * (N - 1)
+    return (k - 1) + (N - 1) * (i != 0 ? (j != 0 ? 3 : 1) : (j != 0 ? 2 : 0)) + offset
+  end
+
+  offset += 4 * (N - 1 + N - 1 + N - 1)
+  if (nbdy == 1) # Face DOF
+    if (ibdy) # On i-normal face
+      return (j - 1) + ((N - 1) * (k - 1)) + (i != 0 ? (N - 1) * (N - 1) : 0) + offset
+    end
+    offset += 2 * (N - 1) * (N - 1)
+    if (jbdy) # On j-normal face
+      return (i - 1) + ((N - 1) * (k - 1)) + (j != 0 ? (N - 1) * (N - 1) : 0) + offset
+    end
+    offset += 2 * (N - 1) * (N - 1)
+    # kbdy, On k-normal face
+    return (i - 1) + ((N - 1) * (j - 1)) + (k != 0 ? (N - 1) * (N - 1) : 0) + offset
+  end
+
+  # nbdy == 0: Body DOF
+  offset += 2 * ( (N - 1) * (N - 1) +
+                 (N - 1) * (N - 1) +
+                 (N - 1) * (N - 1))
+  return offset + (i - 1) + (N - 1) * ((j - 1) + (N - 1) * ((k - 1)))
+end
+
+
+
+
 let
   N = 7
   nelem = 4
@@ -132,104 +190,162 @@ let
               o + L[end, 1, end],
               o + L[end, end, end],
               o + L[1, end, end])
+      tmp = 0
+      @assert tmp == PointIndexFromIJK(0, 0, 0, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(N, 0, 0, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(N, N, 0, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(0, N, 0, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(0, 0, N, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(N, 0, N, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(N, N, N, N); tmp += 1
+      @assert tmp == PointIndexFromIJK(0, N, N, N); tmp += 1
       # edges
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[n, 1, 1])
+      j = 1
+      k = 1
+      for i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[end, n, 1])
+      i = Nq
+      k = 1
+      for j = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[n, end, 1])
+      j = Nq
+      k = 1
+      for i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[1, n, 1])
-      end
-      @printf(io, "\n")
-
-      @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[n, 1, end])
-      end
-      @printf(io, "\n")
-      @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[end, n, end])
-      end
-      @printf(io, "\n")
-      @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[n, end, end])
-      end
-      @printf(io, "\n")
-      @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[1, n, end])
+      i = 1
+      k = 1
+      for j = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
 
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[1, 1, n])
+      j = 1
+      k = Nq
+      for i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[end, 1, n])
+      i = Nq
+      k = Nq
+      for j = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[1, end, n])
+      j = Nq
+      k = Nq
+      for i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for n = 2:Nq-1
-        @printf(io, " %d", o + L[end, end, n])
+      i = 1
+      k = Nq
+      for j = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
+      end
+      @printf(io, "\n")
+
+      @printf(io, "          ")
+      i = 1
+      j = 1
+      for k = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
+      end
+      @printf(io, "\n")
+      @printf(io, "          ")
+      i = Nq
+      j = 1
+      for k = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
+      end
+      @printf(io, "\n")
+      @printf(io, "          ")
+      i = 1
+      j = Nq
+      for k = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
+      end
+      @printf(io, "\n")
+      @printf(io, "          ")
+      i = Nq
+      j = Nq
+      for k = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       # faces
       @printf(io, "          ")
-      for m = 2:Nq-1, n = 2:Nq-1
-        @printf(io, " %d", o + L[1, n, m])
+      i = 1
+      for k = 2:Nq-1, j = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for m = 2:Nq-1, n = 2:Nq-1
-        @printf(io, " %d", o + L[end, n, m])
+      i = Nq
+      for k = 2:Nq-1, j = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for m = 2:Nq-1, n = 2:Nq-1
-        @printf(io, " %d", o + L[n, 1, m])
+      j = 1
+      for k = 2:Nq-1, i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for m = 2:Nq-1, n = 2:Nq-1
-        @printf(io, " %d", o + L[n, end, m])
+      j = Nq
+      for k = 2:Nq-1, i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for m = 2:Nq-1, n = 2:Nq-1
-        @printf(io, " %d", o + L[n, m, 1])
+      k = 1
+      for j = 2:Nq-1, i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       @printf(io, "          ")
-      for m = 2:Nq-1, n = 2:Nq-1
-        @printf(io, " %d", o + L[n, m, end])
+      k = Nq
+      for j = 2:Nq-1, i = 2:Nq-1
+        @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
       # interior
       @printf(io, "          ")
       for k = 2:Nq-1, j = 2:Nq-1, i = 2:Nq-1
         @printf(io, " %d", o + L[i, j, k])
+        @assert tmp == PointIndexFromIJK(i-1, j-1, k-1, N); tmp += 1
       end
       @printf(io, "\n")
     else
